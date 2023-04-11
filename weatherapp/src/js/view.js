ceaf8d727b7033f781d9eeaa5182d2b1a37f9409
Rob Dickson <97182224+dic09005@users.noreply.github.com>
2023-04-11 08:28:11 -0600

    commit

--- weatherapp/src/js/view.js
+++ weatherapp/src/js/view.js
@@ -50,7 +50,7 @@ function saveValue() {
         const response = await fetch(url);
         if (response.ok) {
           const data = await response.json();
-          //console.log(data);
+          console.log(data);
           displayResults(data);
         } else {
             throw Error(await response.text());
